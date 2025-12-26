f3cafb78a10fd55cfda26f14804526dcbe93a9dc
Jitesh Pahwa <jiteshofficial2004@gmail.com>
2025-12-27 00:50:40 +0530

    integrated typescript

--- src/config/server.config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-const SERVER_CONFIG = {
-    PORT : process.env.PORT || 3000,
-}
-
-export default SERVER_CONFIG;
---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { type Request, type Response } from 'express'
+import { SERVER_CONFIG } from './config/server.config'
+
+const app = express()
+
+app.get('/ping', (req: Request, res: Response) => {
+  res.status(200).json({
+    success: true,
+    message: 'pong',
+  })
+})
+
+app.listen(SERVER_CONFIG.PORT, () => {
+  console.log(`Server is running on port ${SERVER_CONFIG.PORT}`)
+})
